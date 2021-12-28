9dd490ba55983447dbedde266ed6f85dd3825aa4
Ramon <jose.diaz@verato.com>
2021-12-27 23:27:09 -0600

    Add script for sequence

--- db/AppUserCreationScript.sql
+++ db/AppUserCreationScript.sql
@@ -1,3 +1,14 @@
+CREATE SEQUENCE public.app_user_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE public.app_user_id_seq
+    OWNER TO postgres;
+
+
 -- Table: public.app_user
 
 -- DROP TABLE public.app_user;
